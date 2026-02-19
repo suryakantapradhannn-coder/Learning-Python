0221a4448c9de26012015eca6797678d1464f483
suryakantapradhannn-coder <253405826+suryakantapradhannn-coder@users.noreply.github.com>
2026-02-19 21:28:40 +0530

    Python input output

---
+++ input_output.py
@@ -0,0 +1,23 @@
+#write a python program to add two numbers
+num1 = int(input('enter first number: '))
+num2 = int(input('enter second number: '))
+sum = num1 + num2
+print('sum is: ',sum)
+
+
+#write a python program to swap two numbers
+num1 = int(input('enter first number: '))
+num2 = int(input('enter second number: '))
+temp = num1
+num1 = num2
+num2 = temp
+print('num1 is: ',num1)
+print('num2 is: ',num2)
+
+# write a python program to find the area of a triangle
+base = int(input('enter base: '))
+height = int(input('enter height: '))
+area = 0.5 * base * height
+print('area is: ',area)
+
+
